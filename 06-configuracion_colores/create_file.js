6eacd0d5a04234e283c72d126ab30d54a86c72b9
guillermo.cadena <guillermo.cadena@karvon.mx>
2021-01-25 21:33:22 -0600

    Adición de configuración de colores en la salida de ejecición del código

---
+++ 06-configuracion_colores/create_file.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+var colors = require('colors');
+
+let crearArchivo = (base, limite) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`la base ${base} no es un número`)
+            return;
+        }
+
+        let data = '';
+
+        for (let i = 0; i < limite; i++) {
+
+            data += `${base} * ${i} : ${i * base} \n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(`El archivo tabla -${base}.txt ha sido creado`);
+                resolve(`tabla - ${ base }.txt`);
+            }
+        });
+
+    });
+}
+
+
+let listarTabla = (base, limite = 10) => {
+
+    console.log('==========================='.red);
+    console.log(`Tabla de  ${base}================`.green);
+    console.log('==========================='.yellow);
+
+    for (let a = 0; a < limite; a++) {
+        console.log(` ${base} * ${limite} :  ${base * limite} `);
+    }
+}
+
+
+//aquí se agregan los objetos que se utilizan de forma global
+module.exports = {
+    crearArchivo: crearArchivo,
+    listarTabla: listarTabla
+}
---
+++ 06-configuracion_colores/ini.js
@@ -0,0 +1,42 @@
+/**
+ * Forma de ejecutar 
+ * 1.-node ini.js --base=5
+ * 2.-node app listar --help
+ * 3.-node ini.js crear --base 10
+ * 4.-node ini.js listar --limite 30 -b 3
+ * 5.-node .\ini.js crear --limite 5 -b 3
+ */
+
+//Se realiza una destructuración
+const { crearArchivo, listarTabla } = require('./create_file.js')
+const argv = require('./config/yargs').argv;
+
+var colors = require('colors');
+var colors = require('colors/safe');
+
+console.log(`argv:`, argv);
+console.log(`Base: ${argv.base}`);
+
+let comando = argv._[0];
+
+switch (comando) {
+
+    case 'listar':
+        console.log('Listar');
+
+        listarTabla(argv.base, argv.limite)
+
+        break;
+
+    case 'crear':
+
+        crearArchivo(argv.base, argv.limite).then(archivo => {
+            //console.log(`Archivo creado: ${archivo}`);
+            console.log(colors.blue('======================='));
+            console.log('Archivo creado: ', colors.green(`${archivo}`));
+        }).catch(err => { console.log(err); });
+
+        break;
+    default:
+        console.log('Comando no reconocido');
+}
